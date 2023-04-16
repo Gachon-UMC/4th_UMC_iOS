e6f16cdc9a2d940f695a6e74aca0597188007cb3
Lee Youjin <kynhun20@gachon.ac.kr>
2023-04-16 22:36:37 +0900

    week3 adding clonecoding

--- zimging/week2/Instagram-iOS/Instagram/UserInterface/Profile/ProfileEditController.swift
+++ zimging/week2/Instagram-iOS/Instagram/UserInterface/Profile/ProfileEditController.swift
@@ -7,10 +7,29 @@
 
 import UIKit
 
-class ProfileEditController: UIViewController {
+protocol SendDataToProfile {
+    func sendDataToP (myName: String, myID: String, myIntro: String, myLink: String)
+}
+
+class ProfileEditController: UIViewController, SendDataToEdit {
+    func SendDataToE(myName: String, myID: String, myIntro: String, myLink: String) {
+        nameText.placeholder = myName
+        userNameText.placeholder = myID
+        introText.placeholder = myIntro
+        linkText.placeholder = myLink
+    }
+    
+    //프로토콜로 delegate 변수 생성
+    var delegate: SendDataToProfile?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+        
+        nameText.borderStyle = .none
+        introText.borderStyle = .none
+        userNameText.borderStyle = .none
+        linkText.borderStyle = .none
     }
     
     @IBOutlet weak var nameText: UITextField!
@@ -18,16 +37,38 @@ class ProfileEditController: UIViewController {
     @IBOutlet weak var introText: UITextField!
     @IBOutlet weak var linkText: UITextField!
     
+    var name = ""
+    var id = ""
+    var intro = ""
+    var link = ""
+    
+    func setup() {
+        nameText.text = name
+        userNameText.text = id
+        introText.text = intro
+        linkText.text = link
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "backProfile" {
             let vc = segue.destination as! ProfileViewController
             vc.name.text = nameText.text!
             vc.id.text = userNameText.text!
             vc.intro.text = introText.text!
-            vc.link.text = linkText.text! }
+            vc.link.text = linkText.text!
+            vc.delegate = self //이건 왜 해주는거지?
+        }
     }
     
-    @IBAction func doneBtn(_ sender: Any) {
+    
+    @IBAction func doneBtn(_ sender: UIButton) {
+        guard let tempName = nameText.text else {return}
+        guard let tempId = userNameText.text else {return}
+        guard let tempIntro = introText.text else {return}
+        guard let tempLink = linkText.text else {return}
         
+        delegate?.sendDataToP(myName: tempName, myID: tempId, myIntro: tempIntro, myLink: tempLink)
+        
+        performSegue(withIdentifier: "backProfile", sender: sender)
     }
 }
